888be0ed784e832f630d85b31a59348bd09808bd
6alileo <6alileo@users.noreply.github.com>
2019-01-03 19:40:49 -0800

    Update fixes.sh

--- scriptmodules/fixes.sh
+++ scriptmodules/fixes.sh
@@ -8,9 +8,3 @@
 # See the LICENSE.md file at the top-level directory of this distribution and
 # at https://raw.githubusercontent.com/Retro-Arena/RetroArena-Setup/master/LICENSE.md
 #
-
-if [[ ! -f $home/.config/update001 ]]; then
-    wget -O $configdir/all/autostart.sh https://pastebin.com/raw/dZTkicwt
-    dos2unix $configdir/all/autostart.sh
-    touch $HOME/.config/update001
-fi
