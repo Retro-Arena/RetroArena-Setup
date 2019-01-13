f18b9e827b9ad2e3f0e0c38701800d472c0cdb0a
6alileo <6alileo@users.noreply.github.com>
2019-01-13 10:29:25 -0800

    don't exit on options

--- scriptmodules/configv/caseconfig.sh
+++ scriptmodules/configv/caseconfig.sh
@@ -30,105 +30,108 @@ function ogst_es() {
 }
 
 function gui_caseconfig() {
-    local cmd=(dialog --backtitle "$__backtitle" --menu "OGST Case Image Selector" 22 86 16)
-    local options=(
-        1 "Console System         ogst-retroarena theme (default)"
-        2 "Motion Blue Boxart     roms/SYSTEM/boxart/ROM.png"
-        3 "Motion Blue Cartart    roms/SYSTEM/cartart/ROM.png"
-        4 "Motion Blue Snap       roms/SYSTEM/snap/ROM.png"
-        5 "Motion Blue Wheel      roms/SYSTEM/wheel/ROM.png"
-        6 "Skyscraper Marquee     roms/SYSTEM/media/marquees/ROM.png"
-        7 "Skyscraper Screenshot  roms/SYSTEM/media/screenshots/ROM.png"
-        8 "Skraper Marquee        roms/SYSTEM/media/marquee/ROM.png"
-        9 "Skraper Screenshot     roms/SYSTEM/media/images/ROM.png"
-        10 "Selph's Marquee        roms/SYSTEM/images/ROM-marquee.png"
-        11 "Selph's Screenshot     roms/SYSTEM/images/ROM-image.jpg"
-        12 "Disable Display        turns off the screen"
-    )
-    local choice=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
-    if [[ -n "$choice" ]]; then
-        case "$choice" in
-            1)
-                ogst_es
-                rm -rf $HOME/.config/ogst*
-                touch $HOME/.config/ogst001
-                touch /usr/local/share/ogst/ogst000
-                printMsgs "dialog" "Enabled Console System\n\nCase image will load from:\n\n/home/pigaming/.emulationstation/ogst_themes/ogst-retroarena/system-SYSTEM.png"
-                ;;
-            2)
-                ogst_es
-                rm -rf $HOME/.config/ogst*
-                touch $HOME/.config/ogst002
-                touch /usr/local/share/ogst/ogst000
-                printMsgs "dialog" "Enabled Motion Blue Boxart\n\nCase image will load from:\n\n/home/pigaming/RetroArena/roms/SYSTEM/boxart/ROM.png"
-                ;;
-            3)
-                ogst_es
-                rm -rf $HOME/.config/ogst*
-                touch $HOME/.config/ogst003
-                touch /usr/local/share/ogst/ogst000
-                printMsgs "dialog" "Enabled Motion Blue Cartart\n\nCase image will load from:\n\n/home/pigaming/RetroArena/roms/SYSTEM/cartart/ROM.png"
-                ;;
-            4)
-                ogst_es
-                rm -rf $HOME/.config/ogst*
-                touch $HOME/.config/ogst004
-                touch /usr/local/share/ogst/ogst000
-                printMsgs "dialog" "Enabled Motion Blue Snap\n\nCase image will load from:\n\n/home/pigaming/RetroArena/roms/SYSTEM/snap/ROM.png"
-                ;;
-            5)
-                ogst_es
-                rm -rf $HOME/.config/ogst*
-                touch $HOME/.config/ogst005
-                touch /usr/local/share/ogst/ogst000
-                printMsgs "dialog" "Enabled Motion Blue Wheel\n\nCase image will load from:\n\n/home/pigaming/RetroArena/roms/SYSTEM/wheel/ROM.png"
-                ;;
-            6)
-                ogst_es
-                rm -rf $HOME/.config/ogst*
-                touch $HOME/.config/ogst006
-                touch /usr/local/share/ogst/ogst000
-                printMsgs "dialog" "Enabled Skyscraper Marquee\n\nCase image will load from:\n\n/home/pigaming/RetroArena/roms/SYSTEM/media/marquees/ROM.png"
-                ;;
-            7)
-                ogst_es
-                rm -rf $HOME/.config/ogst*
-                touch $HOME/.config/ogst007
-                printMsgs "dialog" "Enabled Skyscraper Screenshot\n\nCase image will load from:\n\n/home/pigaming/RetroArena/roms/SYSTEM/media/screenshots/ROM.png"
-                ;;
-            8)
-                ogst_es
-                rm -rf $HOME/.config/ogst*
-                touch $HOME/.config/ogst008
-                printMsgs "dialog" "Enabled Skraper Marquee\n\nCase image will load from:\n\n/home/pigaming/RetroArena/roms/SYSTEM/media/marquee/ROM.png"
-                ;;
-            9)
-                ogst_es
-                rm -rf $HOME/.config/ogst*
-                touch $HOME/.config/ogst009
-                touch /usr/local/share/ogst/ogst000
-                printMsgs "dialog" "Enabled Skraper Screenshot\n\nCase image will load from:\n\n/home/pigaming/RetroArena/roms/SYSTEM/media/images/ROM.png"
-                ;;
-            10)
-                ogst_es
-                rm -rf $HOME/.config/ogst*
-                touch $HOME/.config/ogst010
-                touch /usr/local/share/ogst/ogst000
-                printMsgs "dialog" "Enabled Selph's Marquee\n\nCase image will load from:\n\n/home/pigaming/RetroArena/roms/SYSTEM/images/ROM-marquee.png"
-                ;;
-            11)
-                ogst_es
-                rm -rf $HOME/.config/ogst*
-                touch $HOME/.config/ogst011
-                touch /usr/local/share/ogst/ogst000
-                printMsgs "dialog" "Enabled Selph's Screenshot\n\nCase image will load from:\n\n/home/pigaming/RetroArena/roms/SYSTEM/images/ROM-image.jpg"
-                ;;
-            12)
-                ogst_off
-                rm -rf $HOME/.config/ogst*
-                rm -rf /usr/local/share/ogst/ogst000
-                printMsgs "dialog" "The display is now disabled, including subsequent reboots.\n\nTo re-enable the display, select another option."
-                ;;
-        esac
-    fi
+    while true; do
+        local cmd=(dialog --backtitle "$__backtitle" --menu "OGST Case Image Selector" 22 86 16)
+        local options=(
+            1 "Console System         ogst-retroarena theme (default)"
+            2 "Motion Blue Boxart     roms/SYSTEM/boxart/ROM.png"
+            3 "Motion Blue Cartart    roms/SYSTEM/cartart/ROM.png"
+            4 "Motion Blue Snap       roms/SYSTEM/snap/ROM.png"
+            5 "Motion Blue Wheel      roms/SYSTEM/wheel/ROM.png"
+            6 "Skyscraper Marquee     roms/SYSTEM/media/marquees/ROM.png"
+            7 "Skyscraper Screenshot  roms/SYSTEM/media/screenshots/ROM.png"
+            8 "Skraper Marquee        roms/SYSTEM/media/marquee/ROM.png"
+            9 "Skraper Screenshot     roms/SYSTEM/media/images/ROM.png"
+            10 "Selph's Marquee        roms/SYSTEM/images/ROM-marquee.png"
+            11 "Selph's Screenshot     roms/SYSTEM/images/ROM-image.jpg"
+            12 "Disable Display        turns off the screen"
+        )
+        local choice=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
+        [[ -z "$choice" ]] && break
+        if [[ -n "$choice" ]]; then
+            case "$choice" in
+                1)
+                    ogst_es
+                    rm -rf $HOME/.config/ogst*
+                    touch $HOME/.config/ogst001
+                    touch /usr/local/share/ogst/ogst000
+                    printMsgs "dialog" "Enabled Console System\n\nCase image will load from:\n\n/home/pigaming/.emulationstation/ogst_themes/ogst-retroarena/system-SYSTEM.png"
+                    ;;
+                2)
+                    ogst_es
+                    rm -rf $HOME/.config/ogst*
+                    touch $HOME/.config/ogst002
+                    touch /usr/local/share/ogst/ogst000
+                    printMsgs "dialog" "Enabled Motion Blue Boxart\n\nCase image will load from:\n\n/home/pigaming/RetroArena/roms/SYSTEM/boxart/ROM.png"
+                    ;;
+                3)
+                    ogst_es
+                    rm -rf $HOME/.config/ogst*
+                    touch $HOME/.config/ogst003
+                    touch /usr/local/share/ogst/ogst000
+                    printMsgs "dialog" "Enabled Motion Blue Cartart\n\nCase image will load from:\n\n/home/pigaming/RetroArena/roms/SYSTEM/cartart/ROM.png"
+                    ;;
+                4)
+                    ogst_es
+                    rm -rf $HOME/.config/ogst*
+                    touch $HOME/.config/ogst004
+                    touch /usr/local/share/ogst/ogst000
+                    printMsgs "dialog" "Enabled Motion Blue Snap\n\nCase image will load from:\n\n/home/pigaming/RetroArena/roms/SYSTEM/snap/ROM.png"
+                    ;;
+                5)
+                    ogst_es
+                    rm -rf $HOME/.config/ogst*
+                    touch $HOME/.config/ogst005
+                    touch /usr/local/share/ogst/ogst000
+                    printMsgs "dialog" "Enabled Motion Blue Wheel\n\nCase image will load from:\n\n/home/pigaming/RetroArena/roms/SYSTEM/wheel/ROM.png"
+                    ;;
+                6)
+                    ogst_es
+                    rm -rf $HOME/.config/ogst*
+                    touch $HOME/.config/ogst006
+                    touch /usr/local/share/ogst/ogst000
+                    printMsgs "dialog" "Enabled Skyscraper Marquee\n\nCase image will load from:\n\n/home/pigaming/RetroArena/roms/SYSTEM/media/marquees/ROM.png"
+                    ;;
+                7)
+                    ogst_es
+                    rm -rf $HOME/.config/ogst*
+                    touch $HOME/.config/ogst007
+                    printMsgs "dialog" "Enabled Skyscraper Screenshot\n\nCase image will load from:\n\n/home/pigaming/RetroArena/roms/SYSTEM/media/screenshots/ROM.png"
+                    ;;
+                8)
+                    ogst_es
+                    rm -rf $HOME/.config/ogst*
+                    touch $HOME/.config/ogst008
+                    printMsgs "dialog" "Enabled Skraper Marquee\n\nCase image will load from:\n\n/home/pigaming/RetroArena/roms/SYSTEM/media/marquee/ROM.png"
+                    ;;
+                9)
+                    ogst_es
+                    rm -rf $HOME/.config/ogst*
+                    touch $HOME/.config/ogst009
+                    touch /usr/local/share/ogst/ogst000
+                    printMsgs "dialog" "Enabled Skraper Screenshot\n\nCase image will load from:\n\n/home/pigaming/RetroArena/roms/SYSTEM/media/images/ROM.png"
+                    ;;
+                10)
+                    ogst_es
+                    rm -rf $HOME/.config/ogst*
+                    touch $HOME/.config/ogst010
+                    touch /usr/local/share/ogst/ogst000
+                    printMsgs "dialog" "Enabled Selph's Marquee\n\nCase image will load from:\n\n/home/pigaming/RetroArena/roms/SYSTEM/images/ROM-marquee.png"
+                    ;;
+                11)
+                    ogst_es
+                    rm -rf $HOME/.config/ogst*
+                    touch $HOME/.config/ogst011
+                    touch /usr/local/share/ogst/ogst000
+                    printMsgs "dialog" "Enabled Selph's Screenshot\n\nCase image will load from:\n\n/home/pigaming/RetroArena/roms/SYSTEM/images/ROM-image.jpg"
+                    ;;
+                12)
+                    ogst_off
+                    rm -rf $HOME/.config/ogst*
+                    rm -rf /usr/local/share/ogst/ogst000
+                    printMsgs "dialog" "The display is now disabled, including subsequent reboots.\n\nTo re-enable the display, select another option."
+                    ;;
+            esac
+        fi
+    done
 }
--- scriptmodules/configv/casetheme.sh
+++ scriptmodules/configv/casetheme.sh
@@ -14,44 +14,47 @@ rp_module_desc="Case theme selector for OGST - choose different theme packs when
 rp_module_section="config"
 
 function gui_casetheme() {
-    local cmd=(dialog --backtitle "$__backtitle" --menu "OGST Case Theme Selector" 22 86 16)
-    local options=(
-        1 "Retro-Arena/ogst-retroarena (manning)"
-        2 "Retro-Arena/greatest-hits (waweedman)"
-        3 "Retro-Arena/wiitro-arena (waweedman)"
-        4 "Retro-Arena/hursty (hursty69)"
-    )
-    local choice=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
-    if [[ -n "$choice" ]]; then
-        case "$choice" in
-            1)
-                cd "$HOME/.emulationstation/ogst_themes"
-                rm -rf ogst-retroarena
-                sudo git clone https://github.com/Retro-Arena/ogst-retroarena ogst-retroarena &> /dev/null
-                sudo chown -R pigaming:pigaming "$HOME/.emulationstation/ogst_themes/ogst-retroarena"
-                printMsgs "dialog" "Installed"
-                ;;
-            2)
-                cd "$HOME/.emulationstation/ogst_themes"
-                rm -rf ogst-retroarena
-                sudo git clone https://github.com/Retro-Arena/ogst-greatest-hits ogst-retroarena &> /dev/null
-                sudo chown -R pigaming:pigaming "$HOME/.emulationstation/ogst_themes/ogst-retroarena"
-                printMsgs "dialog" "Installed"
-                ;;
-            3)
-                cd "$HOME/.emulationstation/ogst_themes"
-                rm -rf ogst-retroarena
-                sudo git clone https://github.com/Retro-Arena/ogst-wiitro-arena ogst-retroarena &> /dev/null
-                sudo chown -R pigaming:pigaming "$HOME/.emulationstation/ogst_themes/ogst-retroarena"
-                printMsgs "dialog" "Installed"
-                ;;
-            4)
-                cd "$HOME/.emulationstation/ogst_themes"
-                rm -rf ogst-retroarena
-                sudo git clone https://github.com/Retro-Arena/ogst-hursty ogst-retroarena &> /dev/null
-                sudo chown -R pigaming:pigaming "$HOME/.emulationstation/ogst_themes/ogst-retroarena"
-                printMsgs "dialog" "Installed"
-                ;;
-        esac
-    fi
+    while true; do
+        local cmd=(dialog --backtitle "$__backtitle" --menu "OGST Case Theme Selector" 22 86 16)
+        local options=(
+            1 "Retro-Arena/ogst-retroarena (manning)"
+            2 "Retro-Arena/greatest-hits (waweedman)"
+            3 "Retro-Arena/wiitro-arena (waweedman)"
+            4 "Retro-Arena/hursty (hursty69)"
+        )
+        local choice=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
+        [[ -z "$choice" ]] && break
+        if [[ -n "$choice" ]]; then
+            case "$choice" in
+                1)
+                    cd "$HOME/.emulationstation/ogst_themes"
+                    rm -rf ogst-retroarena
+                    sudo git clone https://github.com/Retro-Arena/ogst-retroarena ogst-retroarena &> /dev/null
+                    sudo chown -R pigaming:pigaming "$HOME/.emulationstation/ogst_themes/ogst-retroarena"
+                    printMsgs "dialog" "Installed"
+                    ;;
+                2)
+                    cd "$HOME/.emulationstation/ogst_themes"
+                    rm -rf ogst-retroarena
+                    sudo git clone https://github.com/Retro-Arena/ogst-greatest-hits ogst-retroarena &> /dev/null
+                    sudo chown -R pigaming:pigaming "$HOME/.emulationstation/ogst_themes/ogst-retroarena"
+                    printMsgs "dialog" "Installed"
+                    ;;
+                3)
+                    cd "$HOME/.emulationstation/ogst_themes"
+                    rm -rf ogst-retroarena
+                    sudo git clone https://github.com/Retro-Arena/ogst-wiitro-arena ogst-retroarena &> /dev/null
+                    sudo chown -R pigaming:pigaming "$HOME/.emulationstation/ogst_themes/ogst-retroarena"
+                    printMsgs "dialog" "Installed"
+                    ;;
+                4)
+                    cd "$HOME/.emulationstation/ogst_themes"
+                    rm -rf ogst-retroarena
+                    sudo git clone https://github.com/Retro-Arena/ogst-hursty ogst-retroarena &> /dev/null
+                    sudo chown -R pigaming:pigaming "$HOME/.emulationstation/ogst_themes/ogst-retroarena"
+                    printMsgs "dialog" "Installed"
+                    ;;
+            esac
+        fi
+    done
 }
