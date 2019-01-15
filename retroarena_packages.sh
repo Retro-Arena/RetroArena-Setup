1dd54e0961d3fd58e3bb7f4d88c44ccd9509b12f
6alileo <6alileo@users.noreply.github.com>
2019-01-14 18:17:28 -0800

    rc3

--- retroarena_packages.sh
+++ retroarena_packages.sh
@@ -9,7 +9,7 @@
 # at https://raw.githubusercontent.com/Retro-Arena/RetroArena-Setup/master/LICENSE.md
 #
 
-__version="1.7.0 BETA 1 RC2"
+__version="1.7.0 BETA 1 RC3"
 
 [[ "$__debug" -eq 1 ]] && set -x
 
