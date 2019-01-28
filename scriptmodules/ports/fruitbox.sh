57567e6831581bdd8fcff184ab5dbda20c7d879a
6alileo <6alileo@users.noreply.github.com>
2019-01-27 17:12:01 -0800

    Update fruitbox.sh

--- scriptmodules/ports/fruitbox.sh
+++ scriptmodules/ports/fruitbox.sh
@@ -167,7 +167,7 @@ function gui_fruitbox() {
                 ;;
             2)
                 verify_gamepad_fruitbox
-                printMsgs "dialog" "Is this the gamepad you'd like to configure?\n\n$device\n\nIf YES, launch Fruitbox from EmulationStation to configure your gamepad.\n\nIf NO, reboot your system with only the gamepad attached and rerun this test.\n\nOnce completed, the config file will be located in $home/fruitbox.btn"
+                printMsgs "dialog" "Is this the gamepad you'd like to configure?\n\n$device\n\nIf YES, launch Fruitbox from EmulationStation to configure your gamepad.\n\nIf NO, reboot your system with only the gamepad attached and rerun this test.\n\nOnce completed, the config file will be located in $home/fruitbox.btn\n\nKnown Issues: laggy input; after gamepad configuration, fruitbox.btn may need to be modified to avoid crashing."
                 ;;
         esac
     done
