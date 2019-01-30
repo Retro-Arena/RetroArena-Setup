0e57bb55935afcce75b929f49f195e10c5a1e87e
6alileo <6alileo@users.noreply.github.com>
2019-01-30 08:45:33 -0800

    disable retroarch-dev until 1.7.6 is released
    meanwhile, retroarch is now pulling from master if installed from source. this will prevent 'ozone' issues if folks start updating from source.

--- scriptmodules/emulators/retroarch.sh
+++ scriptmodules/emulators/retroarch.sh
@@ -10,7 +10,7 @@
 #
 
 rp_module_id="retroarch"
-rp_module_desc="RetroArch v1.7.5 - frontend to the libretro emulator cores - required by all lr-* emulators"
+rp_module_desc="RetroArch - frontend to the libretro emulator cores - required by all lr-* emulators"
 rp_module_licence="GPL3 https://raw.githubusercontent.com/libretro/RetroArch/master/COPYING"
 rp_module_section="core"
 
@@ -38,7 +38,7 @@ function depends_retroarch() {
 }
 
 function sources_retroarch() {
-    gitPullOrClone "$md_build" https://github.com/libretro/RetroArch.git v1.7.5
+    gitPullOrClone "$md_build" https://github.com/libretro/RetroArch.git master
     applyPatch "$md_data/01_hotkey_hack.diff"
     applyPatch "$md_data/02_disable_search.diff"
     applyPatch "$md_data/03_disable_udev_sort.diff"
