185093f00541e0f0dd9bbb1e325289b4e2d74a03
6alileo <6alileo@users.noreply.github.com>
2019-02-07 14:09:58 -0800

    autoupdate toggle switches

--- scriptmodules/supplementary/autoupdate.sh
+++ scriptmodules/supplementary/autoupdate.sh
@@ -10,58 +10,67 @@
 #
 
 rp_module_id="autoupdate"
-rp_module_desc="Automatic updates of Setup Script and Retrohub"
+rp_module_desc="Automatically update scripts and Github repositories"
 rp_module_section="config"
 
-function enable_autoupdate() {
-    sudo systemctl start cron.service
-    crontab -u pigaming "$scriptdir/scriptmodules/supplementary/autoupdate/autoupdate"
+function toggle_autoupdate() {
+    if [[ -e "$home/.config/autoupdate" ]]; then
+        sudo systemctl stop cron.service
+        crontab -u pigaming -r
+        rm -rf "$home/.config/autoupdate"
+        rm -rf "$home/.config/retrohub"
+        rm -rf "$home/.config/setupscript"
+        printMsgs "dialog" "Disabled AutoUpdate Service"
+    else
+        sudo systemctl start cron.service
+        crontab -u pigaming "$scriptdir/scriptmodules/supplementary/autoupdate/autoupdate"
+        touch "$home/.config/autoupdate"
+        printMsgs "dialog" "Enabled AutoUpdate Service"
+    fi
 }
 
-function disable_autoupdate() {
-    sudo systemctl stop cron.service
-    crontab -u pigaming -r
-    rm -rf "$home/.config/setupscript"
-    rm -rf "$home/.config/retrohub"
+function toggle_setupscript() {
+    if [[ -e "$home/.config/setupscript" ]]; then
+        rm -rf "$home/.config/setupscript"
+        printMsgs "dialog" "Disabled RetroArena-Setup AutoUpdate"
+    else
+        touch "$home/.config/setupscript"
+        printMsgs "dialog" "Enabled RetroArena-Setup AutoUpdate\n\nThe update will occur weekly on Sundays at 03:00 UTC."
+    fi
+}
+
+function toggle_retrohub() {
+    if [[ -e "$home/.config/retrohub" ]]; then
+        rm -rf "$home/.config/retrohub"
+        printMsgs "dialog" "Disabled Retrohub AutoUpdate"
+    else
+        touch "$home/.config/retrohub"
+        printMsgs "dialog" "Enabled Retrohub AutoUpdate\n\nThe update will occur weekly on Sundays at 03:00 UTC."
+    fi
 }
 
 function gui_autoupdate() {
-    local cmd=(dialog --backtitle "$__backtitle" --menu "---EXCLUSIVE--- RetroArena Auto Update ---EXCLUSIVE---" 22 86 16)
-    local options=(
-        1 "Enable Auto-update Service"
-        2 "Disable Auto-update Service"
-        3 "Enable RetroArena-Setup Auto-update (Weekly)"
-        4 "Disable RetroArena-Setup Auto-update"
-        5 "Enable Retrohub Auto-update (Weekly)"
-        6 "Disable Retrohub Auto-update"
-    )
-    local choice=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
-    if [[ -n "$choice" ]]; then
-        case "$choice" in
-            1)
-                enable_autoupdate
-                printMsgs "dialog" "Enabled Auto-update Service"
-                ;;
-            2)
-                disable_autoupdate
-                printMsgs "dialog" "Disabled Auto-update Service"
-                ;;
-            3)
-                touch "$home/.config/setupscript"
-                printMsgs "dialog" "Enabled RetroArena-Setup Auto-update.\n\nThe update will occur weekly on Sundays at 03:00 UTC."
-                ;;
-            4)
-                rm -rf "$home/.config/setupscript"
-                printMsgs "dialog" "Disabled RetroArena-Setup Auto-update"
-                ;;
-            5)
-                touch "$home/.config/retrohub"
-                printMsgs "dialog" "Enabled Retrohub Auto-update\n\nThe update will occur weekly on Sundays at 03:00 UTC."
-                ;;
-            6)
-                rm -rf "$home/.config/retrohub"
-                printMsgs "dialog" "Disabled Retrohub Auto-update"
-                ;;
-        esac
-    fi
+    while true; do
+        local options=()        
+        [[ -e "$home/.config/autoupdate" ]] && options+=(A "Disable AutoUpdate Service") || options+=(A "Enable AutoUpdate Service (Required)")
+        [[ -e "$home/.config/setupscript" ]] && options+=(1 "Disable RetroArena-Setup AutoUpdate (Weekly)") || options+=(1 "Enable RetroArena-Setup AutoUpdate")     
+        [[ -e "$home/.config/retrohub" ]] && options+=(2 "Disable Retrohub AutoUpdate (Weekly)") || options+=(2 "Enable Retrohub AutoUpdate")
+        
+        local cmd=(dialog --backtitle "$__backtitle" --menu "AutoUpdate: a RetroArena Exclusive" 22 86 16)
+        local choice=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
+        [[ -z "$choice" ]] && break
+        if [[ -n "$choice" ]]; then
+            case "$choice" in
+                A)
+                    toggle_autoupdate
+                    ;;
+                1)
+                    toggle_setupscript
+                    ;;
+                2)
+                    toggle_retrohub
+                    ;;
+            esac
+        fi
+    done
 }
