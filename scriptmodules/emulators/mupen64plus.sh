03e73330e6438d9437f946126224f29d8e2dbb87
6alileo <6alileo@users.noreply.github.com>
2019-02-09 20:16:19 -0800

    unneeded n64 configs

--- scriptmodules/emulators/mupen64plus.sh
+++ scriptmodules/emulators/mupen64plus.sh
@@ -180,7 +180,7 @@ function install_bin_mupen64plus() {
 
 function configure_mupen64plus() {
     if isPlatform "mali"; then
-         local res
+        local res
         for res in "320x240" "640x480"; do
             local name=""
             [[ "$res" == "640x480" ]] && name="-highres"
@@ -188,7 +188,7 @@ function configure_mupen64plus() {
             addEmulator 0 "${md_id}-gles2rice$name" "n64" "$md_inst/bin/mupen64plus.sh mupen64plus-video-rice %ROM% $res"
 			addEmulator 0 "${md_id}-glide64mk2" "n64" "$md_inst/bin/mupen64plus.sh mupen64plus-video-glide64mk2 %ROM%"
         done
-             addEmulator 1 "${md_id}-auto" "n64" "$md_inst/bin/mupen64plus.sh AUTO %ROM%"
+        addEmulator 1 "${md_id}-auto" "n64" "$md_inst/bin/mupen64plus.sh AUTO %ROM%"
     elif isPlatform "rockpro64"; then
         addEmulator 0 "${md_id}-GLideN64" "n64" "$md_inst/bin/mupen64plus.sh mupen64plus-video-GLideN64 %ROM%"
         addEmulator 0 "${md_id}-glide64mk2" "n64" "$md_inst/bin/mupen64plus.sh mupen64plus-video-glide64mk2 %ROM%"
