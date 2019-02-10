8d17c1bc1376f7a000043646288e010c063b22c6
6alileo <6alileo@users.noreply.github.com>
2019-02-10 09:46:38 -0800

    Update amiberry.sh

--- scriptmodules/emulators/amiberry.sh
+++ scriptmodules/emulators/amiberry.sh
@@ -94,23 +94,21 @@ function configure_amiberry() {
     ln -sf "$configdir/all/retroarch.cfg" "$md_inst/conf/retroarch.cfg"
 
     local system
-    for system in amiga amigacd32; do
-        local config_dir="$md_conf_root/$system/$md_id"
-        
+    for system in amiga amigacd32; do       
         # create whdboot config area
-        moveConfigDir "$md_inst/whdboot" "$config_dir/whdboot"               
+        moveConfigDir "$md_inst/whdboot" "$md_conf_root/$system/$md_id/whdboot"               
         
         # move hostprefs.conf from previous location
-        if [[ -f "$config_dir/conf/hostprefs.conf" ]]; then
-            mv "$config_dir/conf/hostprefs.conf" "$config_dir/whdboot/hostprefs.conf"
+        if [[ -f "$md_conf_root/$system/$md_id/conf/hostprefs.conf" ]]; then
+            mv "$md_conf_root/$system/$md_id/conf/hostprefs.conf" "$md_conf_root/$system/$md_id/whdboot/hostprefs.conf"
         fi
         
         # whdload auto-booter user config - copy default configuration
-        copyDefaultConfig "$md_inst/whdboot-dist/hostprefs.conf" "$config_dir/whdboot/hostprefs.conf"
+        copyDefaultConfig "$md_inst/whdboot-dist/hostprefs.conf" "$md_conf_root/$system/$md_id/whdboot/hostprefs.conf"
         
         # copy game-data, save-data folders and boot-data.zip
-        cp -R "$md_inst/whdboot-dist/"{game-data,save-data,boot-data.zip} "$config_dir/whdboot/"
+        cp -R "$md_inst/whdboot-dist/"{game-data,save-data,boot-data.zip} "$md_conf_root/$system/$md_id/whdboot/"
         
-        chown -R $user:$user "$config_dir/whdboot"
+        chown -R $user:$user "$md_conf_root/$system/$md_id/whdboot"
     done
 }
