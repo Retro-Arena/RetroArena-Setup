51f3a36d707145fe1e5c5d14121f550095ffa404
sikotik <33843126+sikotik@users.noreply.github.com>
2019-03-29 20:44:20 -0700

    test on rockpro64

---
+++ scriptmodules/emulators/ags.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+# This file is part of The RetroArena (TheRA)
+#
+# The RetroArena (TheRA) is the legal property of its developers, whose names are
+# too numerous to list here. Please refer to the COPYRIGHT.md file distributed with this source.
+#
+# See the LICENSE.md file at the top-level directory of this distribution and
+# at https://raw.githubusercontent.com/Retro-Arena/RetroArena-Setup/master/LICENSE.md
+
+rp_module_id="ags"
+rp_module_desc="Adventure Game Studio - Adventure game engine"
+rp_module_help="ROM Extension: .exe\n\nCopy your Adventure Game Studio roms to $romdir/ags"
+rp_module_licence="OTHER https://raw.githubusercontent.com/adventuregamestudio/ags/master/License.txt"
+rp_module_section="sa"
+rp_module_flags="!mali"
+
+function depends_ags() {
+    getDepends xorg pkg-config libaldmb1-dev libfreetype6-dev libtheora-dev libvorbis-dev libogg-dev liballegro4-dev
+}
+
+function sources_ags() {
+    gitPullOrClone "$md_build" https://github.com/adventuregamestudio/ags.git ags3
+   #applyPatch "$md_data/01_fix_build.diff"
+}
+
+function build_ags() {
+    make -C Engine clean
+    make -C Engine
+}
+
+function install_ags() {
+    make -C Engine PREFIX="$md_inst" install
+}
+
+function configure_ags() {
+    mkRomDir "ags"
+
+    # install Eawpatches GUS patch set (see: http://liballeg.org/digmid.html)
+    [[ "$md_mode" == "install" ]] && wget -qO- "http://www.eglebbk.dds.nl/program/download/digmid.dat" | bzcat >"$md_inst/bin/patches.dat"
+
+    if isPlatform "x11"; then
+        addEmulator 1 "$md_id" "ags" "$md_inst/bin/ags --fullscreen %ROM%" "Adventure Game Studio" ".exe"
+    else
+        addEmulator 1 "$md_id" "ags" "xinit $md_inst/bin/ags --fullscreen %ROM%" "Adventure Game Studio" ".exe"
+    fi
+
+    addSystem "ags"
+}
---
+++ scriptmodules/emulators/openmsx.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+# This file is part of The RetroArena (TheRA)
+#
+# The RetroArena (TheRA) is the legal property of its developers, whose names are
+# too numerous to list here. Please refer to the COPYRIGHT.md file distributed with this source.
+#
+# See the LICENSE.md file at the top-level directory of this distribution and
+# at https://raw.githubusercontent.com/Retro-Arena/RetroArena-Setup/master/LICENSE.md
+#
+
+rp_module_id="openmsx"
+rp_module_desc="MSX emulator OpenMSX"
+rp_module_help="ROM Extensions: .rom .mx1 .mx2 .col .dsk .zip\n\nCopy your MSX/MSX2 games to $romdir/msx"
+rp_module_licence="GPL2 https://raw.githubusercontent.com/openMSX/openMSX/master/doc/GPL.txt"
+rp_module_section="prt"
+rp_module_flags="!mali"
+
+function depends_openmsx() {
+    getDepends libsdl1.2-dev libsdl-ttf2.0-dev libglew-dev libao-dev libogg-dev libtheora-dev libxml2-dev libvorbis-dev tcl-dev
+}
+
+function sources_openmsx() {
+    gitPullOrClone "$md_build" https://github.com/openMSX/openMSX.git
+    sed -i "s|INSTALL_BASE:=/opt/openMSX|INSTALL_BASE:=$md_inst|" build/custom.mk
+    sed -i "s|SYMLINK_FOR_BINARY:=true|SYMLINK_FOR_BINARY:=false|" build/custom.mk
+}
+
+function build_openmsx() {
+    rpSwap on 512
+    ./configure
+    make clean
+    make
+    rpSwap off
+}
+
+function install_openmsx() {
+    make install
+    mkdir -p "$md_inst/share/systemroms/"
+    downloadAndExtract "$__archive_url/openmsxroms.tar.gz" "$md_inst/share/systemroms/"
+}
+
+function configure_openmsx() {
+    mkRomDir "msx"
+
+    addEmulator 0 "$md_id" "msx" "xinit $md_inst/bin/openmsx %ROM%"
+    addSystem "msx"
+}
