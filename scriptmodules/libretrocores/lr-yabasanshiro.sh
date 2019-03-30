980a8b2c14ac8b1763b218bbe4ce7e601f971c59
6alileo <6alileo@users.noreply.github.com>
2019-03-30 08:35:49 -0700

    Update lr-yabasanshiro.sh

--- scriptmodules/libretrocores/lr-yabasanshiro.sh
+++ scriptmodules/libretrocores/lr-yabasanshiro.sh
@@ -17,7 +17,8 @@ rp_module_section="lr"
 rp_module_flags="!armv6 !rockpro64"
 
 function sources_lr-yabasanshiro() {
-    gitPullOrClone "$md_build" https://github.com/libretro/yabause.git yabasanshiro
+    #gitPullOrClone "$md_build" https://github.com/libretro/yabause.git yabasanshiro
+    gitPullOrClone "$md_build" https://github.com/devmiyax/yabause.git minimum_linux
     cd "$md_build/yabause"
 }
 
