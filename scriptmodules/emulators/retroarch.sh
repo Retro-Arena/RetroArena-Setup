5befa1a6204cb697b828983f62485a6eebdd9281
6alileo <6alileo@users.noreply.github.com>
2019-04-11 18:54:40 -0700

    retroarch added dependencies for xu4

--- scriptmodules/emulators/retroarch.sh
+++ scriptmodules/emulators/retroarch.sh
@@ -16,6 +16,7 @@ rp_module_section="core"
 
 function depends_retroarch() {
     local depends=(libudev-dev libxkbcommon-dev libsdl2-dev libasound2-dev libusb-1.0-0-dev libpulse-dev)
+    isPlatform "odroid-xu" && depends+=(libavcodec-dev libavformat-dev libavdevice-dev)
     getDepends "${depends[@]}"
     addUdevInputRules
 }
