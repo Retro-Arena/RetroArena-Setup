022631368d7f26049a59892057fa731fda9f870f
6alileo <6alileo@users.noreply.github.com>
2019-05-15 16:57:35 -0700

    Update lr-fbalpha.sh

--- scriptmodules/libretrocores/lr-fbalpha.sh
+++ scriptmodules/libretrocores/lr-fbalpha.sh
@@ -10,26 +10,29 @@
 #
 
 rp_module_id="lr-fbalpha"
-rp_module_desc="Arcade emu - Final Burn Alpha (v0.2.97.44) port for libretro"
+rp_module_desc="Arcade emu - FB Alpha (FB Edition) v0.2.97.44 (WIP) port for libretro"
 rp_module_help="Previously called lr-fba-next\n\ROM Extension: .zip\n\nCopy your FBA roms to\n$romdir/fba or\n$romdir/neogeo or\n$romdir/arcade\n\nFor NeoGeo games the neogeo.zip BIOS is required and must be placed in the same directory as your FBA roms."
-rp_module_licence="NONCOM https://raw.githubusercontent.com/libretro/fbalpha/master/src/license.txt"
+rp_module_licence="NONCOM https://raw.githubusercontent.com/libretro/FBAlphaFB/master/src/license.txt"
 rp_module_section="lr"
 
 function sources_lr-fbalpha() {
-    gitPullOrClone "$md_build" https://github.com/libretro/fbalpha.git
+    gitPullOrClone "$md_build" https://github.com/libretro/FBAlphaFB.git
 }
 
 function build_lr-fbalpha() {
-    ! isPlatform "odroid-n2" && local params="HAVE_NEON=1"
-    make -f makefile.libretro clean
-    make -f makefile.libretro $params
-    md_ret_require="$md_build/fbalpha_libretro.so"
+    cd src/burner/libretro
+    local params=()
+    isPlatform "arm" && params+=(USE_CYCLONE=1)
+    isPlatform "neon" && params+=(HAVE_NEON=1)
+    isPlatform "x86" && isPlatform "64bit" && params+=(USE_X64_DRC=1)
+    make clean
+    make "${params[@]}"
 }
 
 function install_lr-fbalpha() {
     md_ret_files=(
         'fba.chm'
-        'fbalpha_libretro.so'
+        'src/burner/libretro/fbalpha_libretro.so'
         'gamelist.txt'
         'whatsnew.html'
         'preset-example.zip'
@@ -60,16 +63,13 @@ function configure_lr-fbalpha() {
     # Set core options
     setRetroArchCoreOption "fba-diagnostic-input" "Hold Start"
 
-    local def=1
-    isPlatform "armv6" && def=0
     addEmulator 0 "$md_id" "arcade" "$md_inst/fbalpha_libretro.so"
     addEmulator 0 "$md_id-neocd" "arcade" "$md_inst/fbalpha_libretro.so --subsystem neocd"
-    addEmulator 0 "$md_id-neocd" "neocdz" "$md_inst/fbalpha_libretro.so --subsystem neocd"
-    addEmulator $def "$md_id" "neogeo" "$md_inst/fbalpha_libretro.so"
+    addEmulator 1 "$md_id" "neogeo" "$md_inst/fbalpha_libretro.so"
     addEmulator 0 "$md_id-neocd" "neogeo" "$md_inst/fbalpha_libretro.so --subsystem neocd"
-    addEmulator $def "$md_id" "fba" "$md_inst/fbalpha_libretro.so"
+    addEmulator 1 "$md_id-neocd" "neocdz" "$md_inst/fbalpha_libretro.so --subsystem neocd"
+    addEmulator 1 "$md_id" "fba" "$md_inst/fbalpha_libretro.so"
     addEmulator 0 "$md_id-neocd" "fba" "$md_inst/fbalpha_libretro.so --subsystem neocd"
-
     addEmulator 0 "$md_id-pce" "pcengine" "$md_inst/fbalpha_libretro.so --subsystem pce"
     addEmulator 0 "$md_id-sgx" "pcengine" "$md_inst/fbalpha_libretro.so --subsystem sgx"
     addEmulator 0 "$md_id-tg" "pcengine" "$md_inst/fbalpha_libretro.so --subsystem tg"
@@ -77,14 +77,20 @@ function configure_lr-fbalpha() {
     addEmulator 0 "$md_id-sms" "mastersystem" "$md_inst/fbalpha_libretro.so --subsystem sms"
     addEmulator 0 "$md_id-md" "megadrive" "$md_inst/fbalpha_libretro.so --subsystem md"
     addEmulator 0 "$md_id-sg1k" "sg-1000" "$md_inst/fbalpha_libretro.so --subsystem sg1k"
+    addEmulator 0 "$md_id-cv" "coleco" "$md_inst/fbalpha_libretro.so --subsystem cv"
+    addEmulator 0 "$md_id-msx" "msx" "$md_inst/fbalpha_libretro.so --subsystem msx"
+    addEmulator 0 "$md_id-spec" "zxspectrum" "$md_inst/fbalpha_libretro.so --subsystem spec"
 
     addSystem "arcade"
     addSystem "neogeo"
+    addSystem "neocdz"
     addSystem "fba"
-    
     addSystem "pcengine"
     addSystem "gamegear"
     addSystem "mastersystem"
     addSystem "megadrive"
     addSystem "sg-1000"
+    addSystem "coleco"
+    addSystem "msx"
+    addSystem "zxspectrum"
 }
