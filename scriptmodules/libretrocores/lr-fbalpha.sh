16929caf7533ba58a48c8b19214795bfec674b7f
6alileo <6alileo@users.noreply.github.com>
2019-05-21 09:48:36 -0700

    Revert "lr-fbalpha git change"
    This reverts commit b221b978ec119b1fa92301e3c8999c19ba2d3d82.

--- scriptmodules/libretrocores/lr-fbalpha.sh
+++ scriptmodules/libretrocores/lr-fbalpha.sh
@@ -10,13 +10,13 @@
 #
 
 rp_module_id="lr-fbalpha"
-rp_module_desc="Arcade emu - FinalBurn Neo v0.2.97.44 port for libretro"
-rp_module_help="ROM Extension: .zip\n\nCopy your roms to\n$romdir/fba or\n$romdir/neogeo or\n$romdir/arcade\n\nFor NeoGeo games the neogeo.zip BIOS is required and must be placed in the same directory as your roms."
-rp_module_licence="NONCOM https://raw.githubusercontent.com/libretro/FBNeo/master/src/license.txt"
+rp_module_desc="Arcade emu - FB Alpha (FB Edition) v0.2.97.44 port for libretro"
+rp_module_help="ROM Extension: .zip\n\nCopy your FBA roms to\n$romdir/fba or\n$romdir/neogeo or\n$romdir/arcade\n\nFor NeoGeo games the neogeo.zip BIOS is required and must be placed in the same directory as your FBA roms."
+rp_module_licence="NONCOM https://raw.githubusercontent.com/libretro/FBAlphaFB/master/src/license.txt"
 rp_module_section="lr"
 
 function sources_lr-fbalpha() {
-    gitPullOrClone "$md_build" https://github.com/libretro/FBNeo.git
+    gitPullOrClone "$md_build" https://github.com/libretro/FBAlphaFB.git
 }
 
 function build_lr-fbalpha() {
@@ -27,13 +27,13 @@ function build_lr-fbalpha() {
     isPlatform "x86" && isPlatform "64bit" && params+=(USE_X64_DRC=1)
     make clean
     make "${params[@]}"
-    md_ret_require="$md_build/src/burner/libretro/fbneo_libretro.so"
+    md_ret_require="$md_build/src/burner/libretro/fbalpha_libretro.so"
 }
 
 function install_lr-fbalpha() {
     md_ret_files=(
         'fba.chm'
-        'src/burner/libretro/fbneo_libretro.so'
+        'src/burner/libretro/fbalpha_libretro.so'
         'gamelist.txt'
         'whatsnew.html'
         'preset-example.zip'
@@ -62,25 +62,25 @@ function configure_lr-fbalpha() {
     chown $user:$user "$biosdir/fba/hiscore.dat"
 
     # Set core options
-    setRetroArchCoreOption "fbneo-diagnostic-input" "Hold Start"
+    setRetroArchCoreOption "fba-diagnostic-input" "Hold Start"
 
-    addEmulator 0 "$md_id" "arcade" "$md_inst/fbneo_libretro.so"
-    addEmulator 0 "$md_id-neocd" "arcade" "$md_inst/fbneo_libretro.so --subsystem neocd"
-    addEmulator 1 "$md_id" "neogeo" "$md_inst/fbneo_libretro.so"
-    addEmulator 0 "$md_id-neocd" "neogeo" "$md_inst/fbneo_libretro.so --subsystem neocd"
-    addEmulator 1 "$md_id-neocd" "neocdz" "$md_inst/fbneo_libretro.so --subsystem neocd"
-    addEmulator 1 "$md_id" "fba" "$md_inst/fbneo_libretro.so"
-    addEmulator 0 "$md_id-neocd" "fba" "$md_inst/fbneo_libretro.so --subsystem neocd"
-    addEmulator 0 "$md_id-pce" "pcengine" "$md_inst/fbneo_libretro.so --subsystem pce"
-    addEmulator 0 "$md_id-sgx" "pcengine" "$md_inst/fbneo_libretro.so --subsystem sgx"
-    addEmulator 0 "$md_id-tg" "pcengine" "$md_inst/fbneo_libretro.so --subsystem tg"
-    addEmulator 0 "$md_id-gg" "gamegear" "$md_inst/fbneo_libretro.so --subsystem gg"
-    addEmulator 0 "$md_id-sms" "mastersystem" "$md_inst/fbneo_libretro.so --subsystem sms"
-    addEmulator 0 "$md_id-md" "megadrive" "$md_inst/fbneo_libretro.so --subsystem md"
-    addEmulator 0 "$md_id-sg1k" "sg-1000" "$md_inst/fbneo_libretro.so --subsystem sg1k"
-    addEmulator 0 "$md_id-cv" "coleco" "$md_inst/fbneo_libretro.so --subsystem cv"
-    addEmulator 0 "$md_id-msx" "msx" "$md_inst/fbneo_libretro.so --subsystem msx"
-    addEmulator 0 "$md_id-spec" "zxspectrum" "$md_inst/fbneo_libretro.so --subsystem spec"
+    addEmulator 0 "$md_id" "arcade" "$md_inst/fbalpha_libretro.so"
+    addEmulator 0 "$md_id-neocd" "arcade" "$md_inst/fbalpha_libretro.so --subsystem neocd"
+    addEmulator 1 "$md_id" "neogeo" "$md_inst/fbalpha_libretro.so"
+    addEmulator 0 "$md_id-neocd" "neogeo" "$md_inst/fbalpha_libretro.so --subsystem neocd"
+    addEmulator 1 "$md_id-neocd" "neocdz" "$md_inst/fbalpha_libretro.so --subsystem neocd"
+    addEmulator 1 "$md_id" "fba" "$md_inst/fbalpha_libretro.so"
+    addEmulator 0 "$md_id-neocd" "fba" "$md_inst/fbalpha_libretro.so --subsystem neocd"
+    addEmulator 0 "$md_id-pce" "pcengine" "$md_inst/fbalpha_libretro.so --subsystem pce"
+    addEmulator 0 "$md_id-sgx" "pcengine" "$md_inst/fbalpha_libretro.so --subsystem sgx"
+    addEmulator 0 "$md_id-tg" "pcengine" "$md_inst/fbalpha_libretro.so --subsystem tg"
+    addEmulator 0 "$md_id-gg" "gamegear" "$md_inst/fbalpha_libretro.so --subsystem gg"
+    addEmulator 0 "$md_id-sms" "mastersystem" "$md_inst/fbalpha_libretro.so --subsystem sms"
+    addEmulator 0 "$md_id-md" "megadrive" "$md_inst/fbalpha_libretro.so --subsystem md"
+    addEmulator 0 "$md_id-sg1k" "sg-1000" "$md_inst/fbalpha_libretro.so --subsystem sg1k"
+    addEmulator 0 "$md_id-cv" "coleco" "$md_inst/fbalpha_libretro.so --subsystem cv"
+    addEmulator 0 "$md_id-msx" "msx" "$md_inst/fbalpha_libretro.so --subsystem msx"
+    addEmulator 0 "$md_id-spec" "zxspectrum" "$md_inst/fbalpha_libretro.so --subsystem spec"
 
     addSystem "arcade"
     addSystem "neogeo"
