917d2a958f52bf0a675525591a36189159fac322
6alileo <6alileo@users.noreply.github.com>
2019-06-13 19:57:45 -0700

    kodi 18 for n2

--- scriptmodules/emulators/kodi.sh
+++ scriptmodules/emulators/kodi.sh
@@ -14,14 +14,35 @@ rp_module_desc="Kodi - Open source home theatre software. Read the Package Help 
 rp_module_licence="GPL2 https://raw.githubusercontent.com/xbmc/xbmc/master/LICENSE.GPL"
 rp_module_help="The Kodi package is provided AS-IS and WITHOUT support, including for any addons you install.\n\nA keyboard may be required in order to map a gamepad controller.\n\nKnown Issues: Mapping two of the same gamepad controllers will cause instability and may crash."
 rp_module_section="opt"
-rp_module_flags="!osmc !xbian !rockpro64 !odroid-n2"
+rp_module_flags="!osmc !xbian !rockpro64"
+
+function depends_kodi() {
+    local depends
+    isPlatform "odroid-n2" && depends+=(liblzo2-dev libpng-dev libgif-dev libjpeg-dev libpcap-dev libcap-dev python2.7-dev libxml2-dev libxslt1-dev libass-dev libcdio-dev libiso9660-10 libiso9660-dev libcrossguid-dev uuid-dev libcurl4-openssl-dev libfstrcmp-dev libssl-dev libsqlite3-dev libtag1-dev libtinyxml-dev libcec-dev libbluetooth-dev libmicrohttpd-dev liblircclient-dev libsmbclient-dev libnfs-dev libinput-dev libxkbcommon-dev libavahi-client-dev libbluray-dev libmysqlclient-dev liblcms2-dev libsndio-dev libcwiid-dev libplist-dev)
+    getDepends "${depends[@]}"
+}
 
 function install_bin_kodi() {
-    aptInstall kodi-fbdev
+    if isPlatform="odroid-xu"; then
+        aptInstall kodi-fbdev
+        cp "$scriptdir/scriptmodules/emulators/kodi/Kodi.bash" "/usr/bin/kodi"
+        cp "$scriptdir/scriptmodules/emulators/kodi/DialogButtonMenu.xml" "/usr/share/kodi/addons/skin.estuary/xml"
+    fi
+    
+    if isPlatform="odroid-n2"; then
+        cd /usr
+        wget https://github.com/Retro-Arena/binaries/raw/master/odroid-n2/kodi.tar.gz
+        tar -xzf kodi.tar.gz --strip-components=1
+        rm -rf kodi.tar.gz
+        chmod +x /usr/local/bin/kodi*
+        chmod +x /usr/local/bin/TexturePacker
+        chmod -R +x /usr/local/include/kodi
+        chmod -R +x /usr/local/lib/kodi
+        chmod -R +x /usr/local/share/kodi
+        cd -
+    fi
+
     cp -r "$scriptdir/scriptmodules/emulators/kodi/kodi" "$romdir/"
-    cp "$scriptdir/scriptmodules/emulators/kodi/Kodi.bash" "/usr/bin/kodi"
-    cp "$scriptdir/scriptmodules/emulators/kodi/DialogButtonMenu.xml" "/usr/share/kodi/addons/skin.estuary/xml"
-    mkRomDir "kodi"
     chown -R $user:$user "$romdir/kodi"
     moveConfigDir "$home/.kodi" "$md_conf_root/kodi"
     addEmulator 1 "$md_id" "kodi" "kodi %ROM%"
@@ -29,9 +50,20 @@ function install_bin_kodi() {
 }
 
 function remove_kodi() {
-    aptRemove kodi-fbdev
-    aptRemove kodi-fbdev-bin
-    aptRemove kodi-fbdev-data
+    if isPlatform="odroid-xu"; then
+        aptRemove kodi-fbdev
+        aptRemove kodi-fbdev-bin
+        aptRemove kodi-fbdev-data
+    fi
+    
+    if isPlatform="odroid-n2"; then
+        rm -rf /usr/local/bin/kodi*
+        rm -rf /usr/local/bin/TexturePacker
+        rm -rf /usr/local/include/kodi
+        rm -rf /usr/local/lib/kodi
+        rm -rf /usr/local/share/kodi
+    fi
+
     delSystem kodi
     rm -rf "$romdir/kodi"
 }
