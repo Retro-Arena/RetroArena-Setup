242a9da4f34ebc4fdc392b04567632d393335872
6alileo <6alileo@users.noreply.github.com>
2019-06-19 18:41:04 -0700

    Update drastic.sh

--- scriptmodules/emulators/drastic.sh
+++ scriptmodules/emulators/drastic.sh
@@ -14,7 +14,7 @@ rp_module_desc="NDS emu - DraStic"
 rp_module_help="ROM Extensions: .nds .zip\n\nCopy your Nintendo DS roms to $romdir/nds"
 rp_module_licence="PROP"
 rp_module_section="sa"
-rp_module_flags="!odroid-n2"
+rp_module_flags=""
 
 function install_bin_drastic() {
     downloadAndExtract "$__gitbins_url/drastic.tar.gz" "$md_inst" 1
