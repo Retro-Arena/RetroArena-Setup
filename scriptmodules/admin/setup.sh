cca2142928e0af1f1142878ea1eb932a0c1c1b4e
blisstik <blisstik@users.noreply.github.com>
2019-07-23 17:00:50 -0700

    setup - display os version in menu

--- scriptmodules/admin/setup.sh
+++ scriptmodules/admin/setup.sh
@@ -498,7 +498,7 @@ function gui_setup() {
     while true; do
         local commit=$(git -C "$scriptdir" log -1 --pretty=format:"%cr (%h)")
 
-        cmd=(dialog --backtitle "$__backtitle" --title "RetroArena-Setup Script" --cancel-label "Exit" --item-help --help-button --default-item "$default" --menu "Version: $__version\nLast Commit: $commit" 22 76 16)
+        cmd=(dialog --backtitle "$__backtitle" --title "RetroArena-Setup Script" --cancel-label "Exit" --item-help --help-button --default-item "$default" --menu "Version: $__version (running on $__os_desc)\nLast Commit: $commit" 22 76 16)
         options=(
             U "Update Scripts"
             "U Update the RetroArena-Setup script to the latest version."
