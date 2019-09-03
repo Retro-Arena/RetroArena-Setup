2ef3eae7b564d42b9149c182482d07b1a5bb9f48
blisstik <blisstik@users.noreply.github.com>
2019-09-03 10:03:36 -0700

    amiberry: n2 now compiles

--- scriptmodules/emulators/amiberry.sh
+++ scriptmodules/emulators/amiberry.sh
@@ -14,7 +14,7 @@ rp_module_desc="Amiga emulator with JIT support (forked from uae4arm)"
 rp_module_help="ROM Extension: .adf\n\nCopy your Amiga games to $romdir/amiga\n\nCopy the required BIOS files\nkick13.rom\nkick20.rom\nkick31.rom\nto $biosdir"
 rp_module_licence="GPL3 https://raw.githubusercontent.com/midwan/amiberry/master/COPYING"
 rp_module_section="sa"
-rp_module_flags="!x86 !odroid-n2"
+rp_module_flags="!x86"
 
 function _get_platform_bin_amiberry() {
     local choice="$1"
@@ -40,7 +40,7 @@ function depends_amiberry() {
 }
 
 function sources_amiberry() {
-    gitPullOrClone "$md_build" https://github.com/midwan/amiberry.git master
+    gitPullOrClone "$md_build" https://github.com/midwan/amiberry.git dev
 }
 
 function build_amiberry() {
