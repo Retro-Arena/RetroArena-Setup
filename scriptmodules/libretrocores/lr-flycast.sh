d11f66135cdac6aa464a69ef4ee8cf215dba6ec2
blisstik <blisstik@users.noreply.github.com>
2019-09-05 12:13:08 -0700

    fix20

--- scriptmodules/libretrocores/lr-flycast.sh
+++ scriptmodules/libretrocores/lr-flycast.sh
@@ -138,27 +138,3 @@ function configure_lr-flycast() {
     fi
 }
 
-function gui_lr-flycast() {
-    while true; do
-        local options=()
-            [[ -e "$home/.config/au_lr-flycast" ]] && options+=(A "Disable lr-flycast AutoUpdate") || options+=(A "Enable lr-flycast AutoUpdate")
-        local cmd=(dialog --backtitle "$__backtitle" --menu "Choose an option" 22 76 16)
-        local choice=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
-        [[ -z "$choice" ]] && break
-        case "$choice" in
-            A)
-                if [[ -e "$home/.config/au_service" ]]; then
-                    if [[ -e "$home/.config/au_lr-flycast" ]]; then
-                        rm -rf "$home/.config/au_lr-flycast"
-                        printMsgs "dialog" "Disabled lr-flycast AutoUpdate"
-                    else
-                        touch "$home/.config/au_lr-flycast"
-                        printMsgs "dialog" "Enabled lr-flycast AutoUpdate\n\nThe update will occur daily at 05:00 UTC."
-                    fi
-                else
-                    printMsgs "dialog" "ERROR\n\nAutoUpdate Service must be enabled."
-                fi
-                ;;
-        esac
-    done
-}
