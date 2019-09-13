1156d7a9a6e813fe8456c8ed2a2e0d6e5b4248b5
blisstik <blisstik@users.noreply.github.com>
2019-09-13 10:06:00 -0700

    srb2kart md build

--- scriptmodules/ports/srb2kart.sh
+++ scriptmodules/ports/srb2kart.sh
@@ -21,7 +21,7 @@ function depends_srb2kart() {
 function sources_srb2kart() {
     gitPullOrClone "$md_build" https://github.com/STJr/Kart-Public.git
     #downloadAndExtract "$__archive_url/srb2kart-assets.tar.gz" "$md_build"
-    downloadAndExtract "http://s525034382.onlinehome.us/srb2kart-assets.tar.gz" "$md_inst"
+    downloadAndExtract "http://s525034382.onlinehome.us/srb2kart-assets.tar.gz" "$md_build"
 }
 
 function build_srb2kart() {
