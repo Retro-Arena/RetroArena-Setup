0d0f0f21307a5810c944769145c1db519eaa6f72
blisstik <blisstik@users.noreply.github.com>
2019-09-15 21:12:15 -0700

    retroarch 1.7.8.4

--- scriptmodules/emulators/retroarch.sh
+++ scriptmodules/emulators/retroarch.sh
@@ -10,7 +10,7 @@
 #
 
 rp_module_id="retroarch"
-rp_module_desc="RetroArch v1.7.8.3 - stable branch"
+rp_module_desc="RetroArch v1.7.8.4 - stable branch"
 rp_module_licence="GPL3 https://raw.githubusercontent.com/libretro/RetroArch/master/COPYING"
 rp_module_section="core"
 
@@ -23,7 +23,7 @@ function depends_retroarch() {
 
 function sources_retroarch() {
     if [ "$md_id" == "retroarch" ]; then
-        gitPullOrClone "$md_build" https://github.com/libretro/RetroArch.git v1.7.8.3
+        gitPullOrClone "$md_build" https://github.com/libretro/RetroArch.git v1.7.8.4
         applyPatch "$md_data/01_hotkey_hack.diff"
         applyPatch "$md_data/02_disable_search.diff"
     else
