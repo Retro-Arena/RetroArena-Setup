b7fe0dea19c0717ee9b8a42082f42fcc6bcb8c3c
blisstik <42245908+blisstik@users.noreply.github.com>
2019-10-19 12:14:41 -0700

    retroarch v1.7.9.2

--- scriptmodules/emulators/retroarch.sh
+++ scriptmodules/emulators/retroarch.sh
@@ -18,12 +18,11 @@ function depends_retroarch() {
     local depends=(libudev-dev libxkbcommon-dev libsdl2-dev libasound2-dev libusb-1.0-0-dev libpulse-dev)
     isPlatform "odroid-xu" && depends+=(libavcodec-dev libavformat-dev libavdevice-dev)
     getDepends "${depends[@]}"
-    addUdevInputRules
 }
 
 function sources_retroarch() {
     if [ "$md_id" == "retroarch" ]; then
-        gitPullOrClone "$md_build" https://github.com/libretro/RetroArch.git v1.7.8.4
+        gitPullOrClone "$md_build" https://github.com/libretro/RetroArch.git v1.7.9.2
         applyPatch "$md_data/01_hotkey_hack.diff"
         applyPatch "$md_data/02_disable_search.diff"
     else
@@ -33,13 +32,10 @@ function sources_retroarch() {
     fi
 }
 
-function build_retroarch() {  
-    local params=(--disable-sdl --enable-sdl2 --disable-oss --disable-al --disable-jack --disable-qt --enable-pulse)
-    [ "$md_id" == "retroarch-dev" ] && params+=(--disable-opengl1)
-    ! isPlatform "x11" && params+=(--disable-x11 --disable-wayland --disable-kms)
+function build_retroarch() {
+    local params=(--enable-pulse --enable-sdl2  --disable-al --disable-cdrom --disable-discord --disable-easteregg --disable-jack --disable-kms --disable-materialui --disable-opengl1 --disable-oss --disable-sdl --disable-qt --disable-videocore --disable-vulkan --disable-vulkan_display --disable-wayland --disable-x11 --disable-xmb)
     isPlatform "gles" && params+=(--enable-opengles --enable-opengles3)
     isPlatform "mali" && params+=(--enable-mali_fbdev)
-    isPlatform "kms" && params+=(--enable-kms)
     isPlatform "arm" && params+=(--enable-floathard)
     isPlatform "neon" && params+=(--enable-neon)
        
@@ -62,7 +58,6 @@ function install_bin_retroarch() {
 
 function update_assets_retroarch() {
     local dir="$configdir/all/retroarch/assets"
-    # remove if not a git repository for fresh checkout
     [[ ! -d "$dir/.git" ]] && rm -rf "$dir"
     gitPullOrClone "$dir" https://github.com/libretro/retroarch-assets.git
     chown -R $user:$user "$dir"
@@ -70,7 +65,6 @@ function update_assets_retroarch() {
 
 function update_cheats_retroarch() {
     local dir="$configdir/all/retroarch/cheats"
-    # remove if not a git repository for fresh checkout
     [[ ! -d "$dir/.git" ]] && rm -rf "$dir"
     gitPullOrClone "$dir" https://github.com/Retro-Arena/cheats.git
     chown -R $user:$user "$dir"
@@ -78,7 +72,6 @@ function update_cheats_retroarch() {
 
 function update_overlays_retroarch() {
     local dir="$configdir/all/retroarch/overlay"
-    # remove if not a git repository for fresh checkout
     [[ ! -d "$dir/.git" ]] && rm -rf "$dir"
     gitPullOrClone "$dir" https://github.com/libretro/common-overlays.git
     chown -R $user:$user "$dir"
@@ -86,7 +79,6 @@ function update_overlays_retroarch() {
 
 function update_shaders_retroarch() {
     local dir="$configdir/all/retroarch/shaders"
-    # remove if not git repository for fresh checkout
     [[ ! -d "$dir/.git" ]] && rm -rf "$dir"
     gitPullOrClone "$dir" https://github.com/Retro-Arena/glsl-shaders.git
     chown -R $user:$user "$dir"
@@ -94,6 +86,8 @@ function update_shaders_retroarch() {
 
 function configure_retroarch() {
     [[ "$md_mode" == "remove" ]] && return
+    
+    addUdevInputRules
 
     # move / symlink the retroarch configuration
     mkUserDir "$home/.config"
@@ -108,13 +102,9 @@ function configure_retroarch() {
     moveConfigDir "$md_inst/overlays" "$configdir/all/retroarch/overlay"
     moveConfigDir "$md_inst/shader" "$configdir/all/retroarch/shaders"
 
-    # install assets by default
+    # install assets, cheats and shaders by default
     update_assets_retroarch
-
-    # install cheats by default
     update_cheats_retroarch
-
-    # install shaders by default
     update_shaders_retroarch
 
     local config="$(mktemp)"
@@ -128,24 +118,16 @@ function configure_retroarch() {
     # configure default options
     iniConfig " = " '"' "$config"
     iniSet "cache_directory" "/tmp/retroarch"
+    iniSet "core_options_path" "$configdir/all/retroarch-core-options.cfg"
     iniSet "system_directory" "$biosdir"
     iniSet "config_save_on_exit" "false"
-    iniSet "video_aspect_ratio_auto" "true"
+    iniSet "video_scale" "1.0"
+    iniSet "video_threaded" "true"
     iniSet "video_smooth" "false"
-
-    if ! isPlatform "x86"; then
-        iniSet "video_threaded" "true"
-    fi
-
-    iniSet "video_font_size" "12"
-    iniSet "core_options_path" "$configdir/all/retroarch-core-options.cfg"
-    isPlatform "x11" && iniSet "video_fullscreen" "true"
-
-    # set default render resolution to 640x480 for rpi1
-    if isPlatform "rpi1"; then
-        iniSet "video_fullscreen_x" "640"
-        iniSet "video_fullscreen_y" "480"
-    fi
+    iniSet "video_aspect_ratio_auto" "true"
+    iniSet "video_shader_enable" "true"
+    iniSet "auto_shaders_enable" "true"
+    iniSet "audio_out_rate" "44100"
 
     # enable hotkey ("select" button)
     iniSet "input_enable_hotkey" "nul"
@@ -179,33 +161,155 @@ function configure_retroarch() {
     iniSet "input_player1_down" "down"
 
     # input settings
+    iniSet "input_joypad_driver" "udev"
     iniSet "input_autodetect_enable" "true"
     iniSet "auto_remaps_enable" "true"
-    iniSet "input_joypad_driver" "udev"
     iniSet "all_users_control_menu" "true"
+    iniSet "input_reset_btn" "h0down"
 
-    # ozone by default
+    # visual settings
+    iniSet "content_show_add" "false"
+    iniSet "content_show_favorites" "false"
+    iniSet "content_show_history" "false"
+    iniSet "content_show_images" "false"
+    iniSet "content_show_music" "false"
+    iniSet "content_show_netplay" "false"
+    iniSet "content_show_playlists" "false"
+    iniSet "content_show_settings" "true"
+    iniSet "content_show_settings_password" ""
+    iniSet "content_show_video" "false"
+    iniSet "kiosk_mode_enable" "false"
+    iniSet "kiosk_mode_password" ""
+    iniSet "menu_battery_level_enable" "false"
+    iniSet "menu_core_enable" "true"
     iniSet "menu_driver" "ozone"
-
-    # hide online updater menu options
+    iniSet "menu_dynamic_wallpaper_enable" "false"
+    iniSet "menu_enable_widgets" "false"
+    iniSet "menu_entry_hover_color" "ff64ff64"
+    iniSet "menu_entry_normal_color" "ffffffff"
+    iniSet "menu_font_color_blue" "255"
+    iniSet "menu_font_color_green" "255"
+    iniSet "menu_font_color_red" "255"
+    iniSet "menu_footer_opacity" "1.000000"
+    iniSet "menu_framebuffer_opacity" "0.500000"
+    iniSet "menu_header_opacity" "1.000000"
+    iniSet "menu_horizontal_animation" "true"
+    iniSet "menu_left_thumbnails" "0"
+    iniSet "menu_linear_filter" "true"
+    iniSet "menu_mouse_enable" "true"
+    iniSet "menu_navigation_browser_filter_supported_extensions_enable" "true"
+    iniSet "menu_navigation_wraparound_enable" "true"
+    iniSet "menu_pause_libretro" "true"
+    iniSet "menu_pointer_enable" "false"
+    iniSet "menu_shader_pipeline" "0"
+    iniSet "menu_show_advanced_settings" "true"
+    iniSet "menu_show_configurations" "true"
     iniSet "menu_show_core_updater" "false"
+    iniSet "menu_show_help" "false"
+    iniSet "menu_show_information" "false"
+    iniSet "menu_show_latency" "false"
+    iniSet "menu_show_legacy_thumbnail_updater" "false"
+    iniSet "menu_show_load_content" "false"
+    iniSet "menu_show_load_core" "false"
     iniSet "menu_show_online_updater" "false"
-
-    # swap A/B buttons based on ES configuration
+    iniSet "menu_show_overlays" "false"
+    iniSet "menu_show_quit_retroarch" "true"
+    iniSet "menu_show_reboot" "false"
+    iniSet "menu_show_restart_retroarch" "false"
+    iniSet "menu_show_rewind" "false"
+    iniSet "menu_show_shutdown" "true"
+    iniSet "menu_show_sublabels" "true"
     iniSet "menu_swap_ok_cancel_buttons" "$es_swap"
-
+    iniSet "menu_throttle_framerate" "true"
+    iniSet "menu_thumbnails" "3"
+    iniSet "menu_timedate_enable" "true"
+    iniSet "menu_timedate_style" "1"
+    iniSet "menu_title_color" "ff64ff64"
+    iniSet "menu_unified_controls" "true"
+    iniSet "menu_use_preferred_system_color_theme" "false"
+    iniSet "menu_wallpaper" ""
+    iniSet "menu_wallpaper_opacity" "0.500000"
+    iniSet "ozone_menu_color_theme" "1"
+    iniSet "quick_menu_show_add_to_favorites" "false"
+    iniSet "quick_menu_show_cheats" "true"
+    iniSet "quick_menu_show_close_content" "true"
+    iniSet "quick_menu_show_controls" "true"
+    iniSet "quick_menu_show_download_thumbnails" "false"
+    iniSet "quick_menu_show_information" "false"
+    iniSet "quick_menu_show_options" "true"
+    iniSet "quick_menu_show_recording" "false"
+    iniSet "quick_menu_show_reset_core_association" "false"
+    iniSet "quick_menu_show_restart_content" "true"
+    iniSet "quick_menu_show_resume_content" "true"
+    iniSet "quick_menu_show_save_content_dir_overrides" "true"
+    iniSet "quick_menu_show_save_core_overrides" "true"
+    iniSet "quick_menu_show_save_game_overrides" "true"
+    iniSet "quick_menu_show_save_load_state" "true"
+    iniSet "quick_menu_show_set_core_association" "false"
+    iniSet "quick_menu_show_shaders" "true"
+    iniSet "quick_menu_show_start_recording" "false"
+    iniSet "quick_menu_show_start_streaming" "false"
+    iniSet "quick_menu_show_streaming" "false"
+    iniSet "quick_menu_show_take_screenshot" "false"
+    iniSet "quick_menu_show_undo_save_load_state" "false"
+    iniSet "quit_press_twice" "false"
+    iniSet "rgui_aspect_ratio" "2"
+    iniSet "rgui_aspect_ratio_lock" "1"
+    iniSet "rgui_background_filler_thickness_enable" "true"
+    iniSet "rgui_border_filler_enable" "false"
+    iniSet "rgui_border_filler_thickness_enable" "false"
+    iniSet "rgui_browser_directory" "$romdir"
+    iniSet "rgui_config_directory" "~/.config/retroarch/config"
+    iniSet "rgui_extended_ascii" "false"
+    iniSet "rgui_inline_thumbnails" "false"
+    iniSet "rgui_internal_upscale_level" "0"
+    iniSet "rgui_menu_color_theme" "20"
+    iniSet "rgui_menu_theme_preset" ""
+    iniSet "rgui_particle_effect" "5"
+    iniSet "rgui_show_start_screen" "false"
+    iniSet "rgui_swap_thumbnails" "false"
+    iniSet "rgui_thumbnail_delay" "0"
+    iniSet "rgui_thumbnail_downscaler" "0"
+    iniSet "settings_show_achievements" "true"
+    iniSet "settings_show_ai_service" "true"
+    iniSet "settings_show_audio" "true"
+    iniSet "settings_show_configuration" "true"
+    iniSet "settings_show_core" "true"
+    iniSet "settings_show_directory" "true"
+    iniSet "settings_show_drivers" "true"
+    iniSet "settings_show_frame_throttle" "true"
+    iniSet "settings_show_input" "true"
+    iniSet "settings_show_latency" "true"
+    iniSet "settings_show_logging" "true"
+    iniSet "settings_show_network" "true"
+    iniSet "settings_show_onscreen_display" "true"
+    iniSet "settings_show_playlists" "false"
+    iniSet "settings_show_power_management" "false"
+    iniSet "settings_show_recording" "true"
+    iniSet "settings_show_saving" "true"
+    iniSet "settings_show_user" "true"
+    iniSet "settings_show_user_interface" "true"
+    iniSet "settings_show_video" "true"
+    iniSet "video_font_enable" "true"
+    iniSet "video_font_path" ""
+    iniSet "video_font_size" "20.000000"
+    iniSet "video_message_color" "33ff00"
+    iniSet "video_message_pos_x" "0.050000"
+    iniSet "video_message_pos_y" "0.050000"
+    iniSet "video_msg_bgcolor_blue" "0"
+    iniSet "video_msg_bgcolor_enable" "false"
+    iniSet "video_msg_bgcolor_green" "0"
+    iniSet "video_msg_bgcolor_opacity" "1.000000"
+    iniSet "video_msg_bgcolor_red" "0"
+   
     copyDefaultConfig "$config" "$configdir/all/retroarch.cfg"
     rm "$config"
 
-    # if no menu_driver is set, force OZONE
-    iniConfig " = " '"' "$configdir/all/retroarch.cfg"
-    iniGet "menu_driver"
-    [[ -z "$ini_value" ]] && iniSet "menu_driver" "ozone"
-
-    # if no menu_unified_controls is set, force it on so that keyboard player 1 can control
-    # the OZONE menu which is important for arcade sticks etc that map to keyboard inputs
-    iniGet "menu_unified_controls"
-    [[ -z "$ini_value" ]] && iniSet "menu_unified_controls" "true"
+    # force settings on existing configs
+    _init_config_option_retroarch "menu_driver" "ozone"
+    _init_config_option_retroarch "menu_unified_controls" "true"
+    _init_config_option_retroarch "quit_press_twice" "false"
+    _init_config_option_retroarch "video_shader_enable" "true"
 
     # remapping hack for old 8bitdo firmware
     addAutoConf "8bitdo_hack" 0
@@ -335,4 +439,14 @@ function gui_retroarch() {
         esac
 
     done
+}
+
+function _set_config_option_retroarch() {
+    local option="$1"
+    local value="$2"
+    iniConfig " = " "\"" "$configdir/all/retroarch.cfg"
+    iniGet "$option"
+    if [[ -z "$ini_value" ]]; then
+        iniSet "$option" "$value"
+    fi
 }
