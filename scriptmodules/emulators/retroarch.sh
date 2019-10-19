7be4e2de997596f97f3b9a291683ee5f1157b848
blisstik <42245908+blisstik@users.noreply.github.com>
2019-10-19 12:11:48 -0700

    retroarch v1.7.9.2

--- scriptmodules/emulators/retroarch.sh
+++ scriptmodules/emulators/retroarch.sh
@@ -22,7 +22,7 @@ function depends_retroarch() {
 
 function sources_retroarch() {
     if [ "$md_id" == "retroarch" ]; then
-        gitPullOrClone "$md_build" https://github.com/libretro/RetroArch.git v1.7.8.4
+        gitPullOrClone "$md_build" https://github.com/libretro/RetroArch.git v1.7.9.2
         applyPatch "$md_data/01_hotkey_hack.diff"
         applyPatch "$md_data/02_disable_search.diff"
     else
